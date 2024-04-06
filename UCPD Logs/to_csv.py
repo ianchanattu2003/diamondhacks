1777f2f5aa5c22dfeec3f5e51e6c36964dc7ce4c
Isaac Anchanattu <ianchanattu@ucsd.edu>
2024-04-06 16:54:26 -0700

    Added to to_csv.py to parse txt data, might want to substring lines to remove labels

--- UCPD Logs/to_csv.py
+++ UCPD Logs/to_csv.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 situation = []
 location = []
@@ -9,10 +10,31 @@ occurTime = []
 summary = []
 disposition = []
 
-
-with open("output.txt", 'r') as file:
+count = 0
+with open("output.txt", 'r',errors='ignore') as file:
     for line in file:
-        print("")
+        if(count == 0):
+            situation.append(line)
+            count= 1
+        elif(count == 1):
+            location.append(line)
+            count = 2
+        elif(count == 2):
+            if(line.find("Date Reported") != -1):
+                reportDate.append(line)
+            elif(line.find("Incident/Case#") != -1):
+                caseNum.append(line)
+            elif(line.find("Date Occurred") != -1):
+                occurDate.append(line)
+            elif(line.find("Time Occurred") != -1):
+                occurTime.append(line)
+            elif(line.find("Summary") != -1):
+                summary.append(line)
+            elif(line.find("Disposition") != -1):
+                disposition.append(line)
+                count = 0
+        
 
 df = pd.DataFrame({"Situation": situation, "Location": location, "Date Reported": reportDate, "Incident/Case#": caseNum, "Date Occured": occurDate, "Time Occured": occurTime, "Summary": summary, "Disposition": disposition})
+os.makedirs('data', exist_ok=True)  
 df.to_csv('data/logs.csv', index=False)
