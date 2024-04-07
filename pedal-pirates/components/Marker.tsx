b5a395ab2905abf5f78b5870a4a0e8a819a10062
Sean <ussa.say@gmail.com>
2024-04-07 15:14:58 -0700

    display emoji

--- pedal-pirates/components/Marker.tsx
+++ pedal-pirates/components/Marker.tsx
@@ -1,14 +1,35 @@
 import { Report } from '@/lib/parse-data'
 import { CSSProperties } from 'react'
 
+function displayObject (object: string): string | null {
+  object = object.toLowerCase()
+  if (object.includes('bicycle')) {
+    return '🚲'
+  } else if (object.includes('scooter')) {
+    return '🛴'
+  } else {
+    return null
+  }
+}
+
 export type MarkerProps = {
   reports: Report[]
   style?: CSSProperties
 }
 export function Marker ({ reports, style }: MarkerProps) {
+  const hasEmoji =
+    reports.length === 1 && displayObject(reports[0].object) !== null
+
   return (
-    <button className={`report report-${reports.length}`} style={style}>
-      {reports.length === 1 ? '1' : reports.length}
+    <button
+      className={`report report-${reports.length} ${
+        hasEmoji ? 'has-emoji' : ''
+      }`}
+      style={style}
+    >
+      {reports.length === 1
+        ? displayObject(reports[0].object) ?? '1'
+        : reports.length}
     </button>
   )
 }
